beda3e9177b52d87a93b39b2055ddc3992036941
Matheus Schneider Tallmann <mtallmann@furb.br>
2023-03-09 21:40:28 -0300

    Adicionando Exercicios

---
+++ src/Unidade2.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Unidade2 {
+    
+    public static void main(String[] args)
+    {    
+        Exercise2(args);
+    }
+
+    public static void Exercise1(String[] args)
+    // Leia 2 valores inteiros e imprima sua soma.
+    {
+        Scanner readLine = new Scanner(System.in);
+
+        System.out.println("Insira um número inteiro:");
+        int number1 = readLine.nextInt();
+        System.out.println("Insira um número inteiro:");
+        int number2 = readLine.nextInt();
+           
+        readLine.close();
+        int sum = number1 + number2;
+
+        System.out.println("A soma de "+number1+" e "+number2+" é: "+sum);
+    }
+
+    public static void Exercise2(String[] args)
+    // Leia 2 valores inteiros e imprima sua multiplicação.
+    {
+        Scanner readLine = new Scanner(System.in);
+
+        System.out.println("Insira um número inteiro:");
+        int number1 = readLine.nextInt();
+        System.out.println("Insira um número inteiro:");
+        int number2 = readLine.nextInt();
+           
+        readLine.close();
+        int sum = number1 * number2;
+
+        System.out.println("A multiplicação de "+number1+" e "+number2+" é: "+sum);
+    }
+
+    public static void Exercise3(String[] args)
+    // Calcule a área da circunferência elevando o valor de raio ao quadrado e multiplicando por π (π = 3.14159).
+    {
+        Scanner readLine = new Scanner(System.in);
+
+        System.out.println("Insira um número inteiro:");
+        int number1 = readLine.nextInt();
+        System.out.println("Insira um número inteiro:");
+        int number2 = readLine.nextInt();
+           
+        readLine.close();
+        int sum = number1 * number2;
+
+        System.out.println("A multiplicação de "+number1+" e "+number2+" é: "+sum);
+    }
+}
